1c5707415b69c012823672cf68776d1880eaa8bc
Lucas <lucasmachado010207@gmail.com>
2023-09-15 18:11:17 -0300

    rotas http de crud

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 // Importar pacote do express
+const { request } = require('express');
 const express = require('express');
 // Instanciar o express na variavel app
 const app = express();
 // Importar um pacote dotenv
 const dotenv = require('dotenv').config();
-// defnir a porta do servidor
+// Defnir a porta do servidor
 const PORT = process.env.PORT || 3005;
+
+//Criando rota get
+app.get('/api', (request, response) => {
+    response.send('Retorno de informações de banco de dados');
+});
+app.post('/api', (request, response) => {
+    response.send('Metodo utilizado para salvar informações!');
+});
+app.put('/api/:id', (request, response) => {
+    response.send('Metodo utilizado para salvar informações!');
+});
+app.delete('/api/:id', (request, response) => {
+    response.send('Metodo utilizado para salvar informações!');
+});
 
 // Testar servidor 
 app.listen(PORT, () => console.log(`Running at port ${PORT}`))
