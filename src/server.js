39eac3ace2fdbd49c08ef235219b577b1d1b25ca
Lucas <lucasmachado010207@gmail.com>
2023-11-01 18:16:17 -0300

    Oi Ney

--- src/server.js
+++ src/server.js
@@ -3,7 +3,5 @@ const app = require('./app');
 // Importar a porta do servidor
 const PORT = app.get('port');
 
-
-
 // Testar servidor 
 app.listen(PORT, () => console.log(`Running at port ${PORT}`))
