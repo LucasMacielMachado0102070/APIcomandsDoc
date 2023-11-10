832e1306de02dadf4e8bcbc095871e5a91351ac0
Lucas <lucasmachado010207@gmail.com>
2023-11-10 18:07:30 -0300

    funcao cadastrar

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -1,21 +1,66 @@
+// Importar a conexão com o banco de dados
 const connection = require('../config/db');
 
+// Função async: realiza uma requisição des forma paralela ao sistema
+// request: nesta variável recuperamos os dados enviados na requisição
+// response: tem a responsabilidade de retornar as informações para quem requisitou
 async function listarUsuarios(request, response) {
+    // Variável para executar a consulta no banco
     const query = 'SELECT * FROM alunos;';
 
+// err: retorna erros na execução
+// results: retorna a ação realizada com sucesso 
     connection.query(query, (err, results) => {
         if(results) {
             response
             .status(200)
             .json({
                 sucess: true,
-                message: "Sucesso lista de alunos",
+                message: "Sucesso! Lista de alunos",
                 data: results
             });
         }else {
-
+            response
+            .status(200)
+            .json({
+                sucess: true,
+                message: "Ooops! Não foi pussível listar as informações solicitadas!",
+                data: err
+            });
         }
     });
 }
 
-module.exports = listarUsuarios;
+async function cadastrarAluno(request, response) {
+// 1° passo: recuperar varáveis
+// 2° passo: montar query para inserir os dados 
+// 3° passo: tentar executar a ação no banco
+// 4° passo: definir retornos da requisição
+
+// Recuperando dados da requisição
+const params = Array(
+    request.body.nome, 
+    request.body.dt_nascimente,
+    request.body.time_do_coracao
+);
+
+
+const query = 'INSERT INTO alunos(nome, dt_nascimento, time_do_coracao) values(?, ?, ?);';
+
+connection.query(query, params, (err, results) => {
+    if (results) {
+        response
+            .status(201)
+            .json({
+                sucess: true,
+                message: "Sucesso! Auno cadastrado",
+                data: results
+            })
+    }
+})
+}
+
+module.exports = {
+    listarUsuarios,
+    cadastrarAluno
+}
